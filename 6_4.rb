ce5cfef30d38ba03408e2dd6e8ce42f60695319f
kinasu <kinasu@gmail.com>
2017-03-15 23:22:45 +0300

    initial commit

---
+++ 6_4.rb
@@ -0,0 +1,16 @@
+require 'colorize'
+comp = %w(rock paper sciser)
+puts 'Choise
+1. Rock
+2. Papper
+3. Sciser'.light_blue
+comp_choise = comp.sample
+user_choise = comp[gets.chomp.to_i - 1]
+
+if comp_choise == user_choise
+  puts 'DRAW'.yellow
+elsif comp_choise == 'rock' && user_choise == 'sciser' || comp_choise == 'paper' && user_choise == 'rock' || comp_choise == 'sciser' && user_choise == 'paper'
+  puts 'YOU LOOSE!\nAI choise #{comp_choise}\nYou choise #{user_choise}'.red
+else
+  puts 'YOU WINNER!\nAI choise #{comp_choise}\nYou choise #{user_choise}'.green
+end
