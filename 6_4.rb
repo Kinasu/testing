b35a67ce3c9c449cb1753d825c0b2b479f5b3b1e
Kinasu <ashtoreet@gmail.com>
2017-07-20 17:28:10 +0300

    fix bug

--- 6_4.rb
+++ 6_4.rb
@@ -1,16 +1,16 @@
 require 'colorize'
 comp = %w(rock paper sciser)
-puts 'Choise
+puts "Choise
 1. Rock
 2. Papper
-3. Sciser'.light_blue
+3. Sciser".light_blue
 comp_choise = comp.sample
 user_choise = comp[gets.chomp.to_i - 1]
 
 if comp_choise == user_choise
-  puts 'DRAW'.yellow
+  puts "DRAW".yellow
 elsif comp_choise == 'rock' && user_choise == 'sciser' || comp_choise == 'paper' && user_choise == 'rock' || comp_choise == 'sciser' && user_choise == 'paper'
-  puts 'YOU LOOSE!\nAI choise #{comp_choise}\nYou choise #{user_choise}'.red
+  puts "YOU LOOSE!\nAI choise #{comp_choise}\nYou choise #{user_choise}".red
 else
-  puts 'YOU WINNER!\nAI choise #{comp_choise}\nYou choise #{user_choise}'.green
+  puts "YOU WINNER!\nAI choise #{comp_choise}\nYou choise #{user_choise}".green
 end
